9acb107520d8b3be8843cdc035eca49189b5a429
Abhigyan Bhadra <bhadraabhigyan@gmail.com>
2019-08-26 16:25:36 +0530

    Update qs_5.java

--- qs_5.java
+++ qs_5.java
@@ -10,22 +10,22 @@ public class qs_5 {
              }
              if(sal>=15000 && sal<30000)
              {
-            	  tax=2250+(sal*0.18);
+            	  tax=2250+((sal-15000)*0.18);
             	  System.out.println("Tax due "+tax);
              }
              if(sal>=30000 && sal<50000)
              {
-            	 tax=5400+(sal*0.22);
+            	 tax=5400+((sal-30000)*0.22);
             	 System.out.println("Tax due "+tax);
              }
              if(sal>=50000 && sal<80000)
              {
-            	 tax=11000+(sal*0.27);
+            	 tax=11000+((sal-50000)*0.27);
             	 System.out.println("Tax due "+tax);           	 
              }
              if(sal>=80000 && sal<=150000)
              {
-            	 tax=21600+(sal*0.33);
+            	 tax=21600+((sal-80000)*0.33);
             	 System.out.println("Tax due "+tax);;
              }
              if(sal>150000)
